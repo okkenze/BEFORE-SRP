ea2849f8ace2bfef77bcab840772e6287dfc266c
Okechukwu Nmema <Okechukwu.Nmema@fcmb.com>
2021-01-07 19:53:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BEFORESRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = new ReportGenerator();
+            var formatedResult = generator.GenerateReportASString();
+
+            Console.WriteLine();
+            Console.WriteLine(formatedResult);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReportGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BEFORESRP
+{
+    public class ReportGenerator
+    {
+        private List<User> _user;
+
+        public ReportGenerator()
+        {
+            _user = new List<User>();
+        }
+
+
+        public string GenerateReportASString()
+        {
+            //get data from built in UserEntry
+            _user.Add(new User() { FirstName = "Okechukwu", LastName = "Nmema" });
+            
+
+            //format data into json
+            return JsonConvert.SerializeObject(_user);
+
+        }
+        //User model
+        private class User
+        {
+           
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
